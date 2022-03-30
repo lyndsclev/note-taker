07b4a0fd14a62c5460f35a94abcd4e38bdadccd4
lyndsclev <lyndsclev@gmail.com>
2022-03-30 12:20:25 -0400

    data validating

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // required 
+const fs = require('fs');
+const path = require('path');
 const express = require('express'); 
+const req = require('express/lib/request');
 const { notes } = require('./db/db'); 
 
 const PORT = process.env.PORT || 3001; 
@@ -9,6 +12,29 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
+// create note
+function createNote(body, notesArray) {
+    const note = body; 
+    notesArray.push(note); 
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'), 
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}; 
+
+// validate input
+function validateNote(note) {
+    if(!note.title) {
+        return false; 
+    }
+    if(!note.text) {
+        return false;
+    }
+    return true; 
+}; 
+
 // get notes 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -16,8 +42,18 @@ app.get('/api/notes', (req, res) => {
 
 // post notes 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+
+    // set id based on array index
+    req.body.id = notes.length.toString();
+
+    // if data is empty, send alert
+    if(!validateNote(req.body)) {
+        res.status(400).send('Please include information for both Note Title and Note Text'); 
+    } else {
+        // add note to json file and notes array
+        const note = createNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
